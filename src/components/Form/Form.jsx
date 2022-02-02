122f2c3aee6f2008587a43abca3f67eeed22a2e3
ChernyshevaNetology <chernyshevak87@mail.ru>
2022-02-02 15:14:42 +0200

    refactor according to comments

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,43 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "App.css";
 
-const Form = ({ handleSubmit, handleChange, inputName }) => (
-  <form className="form" onSubmit={handleSubmit}>
-    <label htmlFor="name1" className="label-name">
-      {" "}
-      Название{" "}
-    </label>
-    <input
-      id="name1"
-      ref={inputName}
-      className="input-name"
-      defaultValue=""
-      type="text"
-      onChange={handleChange}
-      name="name"
-      required={true}
-      placeholder="Город"
-    />
+const Form = ({ handleSubmit }) => {
+  const [dataValue, setDataValue] = useState({});
 
-    <label htmlFor="timeZone" className="label-timezone">
-      {" "}
-      Временная зона
-    </label>
-    <input
-      id="timeZone"
-      className="input-timezone"
-      type="number"
-      onChange={handleChange}
-      name="timeZone"
-      defaultValue=""
-      required={true}
-      placeholder="+ часов"
-    />
+  const handleChange = ({ target: { name, value } }) => {
+    setDataValue({ ...dataValue, [name]: value });
+  };
 
-    <button className="btn-submit" onSubmit={handleSubmit}>
-      Добавить
-    </button>
-  </form>
-);
+  return (
+    <form className="form" onSubmit={(e) => handleSubmit(e, dataValue)}>
+      <label htmlFor="name1" className="label-name">
+        Название
+      </label>
+      <input
+        id="name1"
+        autoFocus
+        className="input-name"
+        defaultValue=""
+        type="text"
+        onChange={handleChange}
+        name="name"
+        required={true}
+        placeholder="Город"
+      />
+
+      <label htmlFor="timeZone" className="label-timezone">
+        Временная зона
+      </label>
+      <input
+        id="timeZone"
+        className="input-timezone"
+        type="number"
+        onChange={handleChange}
+        name="timeZone"
+        defaultValue=""
+        required={true}
+        placeholder="+ часов"
+      />
+      <button className="btn-submit">Добавить</button>
+    </form>
+  );
+};
 
 export { Form };
--- src/components/WatchCard/WatchCard.jsx
+++ src/components/WatchCard/WatchCard.jsx
@@ -1,21 +1,21 @@
 import { addHours, format } from "date-fns";
-import React from "react";
+import React, { useState } from "react";
 import "App.css";
 
-const WatchCard = ({ id, name, timeZone, handleRemove }) => (
-  <div key={id} className="watch-card">
-    <i
-      className="fa fa-times-circle-o"
-      aria-hidden="true"
-      onClick={() => handleRemove(id)}
-    />
-    <i className="titleWatch">{name}</i>
-    <i className="dataWatch">
-      {" "}
-      {format(addHours(new Date(), timeZone), "HH:mm:ss")}
-      {""}+{timeZone}
-    </i>
-  </div>
-);
+const WatchCard = ({ id, name, timeZone, handleRemove }) => {
+  return (
+    <div key={id} className="watch-card">
+      <i
+        className="fa fa-times-circle-o"
+        aria-hidden="true"
+        onClick={() => handleRemove(id)}
+      />
+      <i className="titleWatch">{name}</i>
+      <i className="dataWatch">
+        {format(addHours(new Date(), timeZone), "HH:mm:ss")}+{timeZone}
+      </i>
+    </div>
+  );
+};
 
 export { WatchCard };
